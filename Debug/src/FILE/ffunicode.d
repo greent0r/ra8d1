780fc8077cafa6607ece33e28748099c1aa384e9
greent0r <156403048+greent0r@users.noreply.github.com>
2025-07-23 14:49:30 +0800

    Delete Debug directory

--- Debug/src/FILE/ffunicode.d
+++
@@ -1,4 +0,0 @@
-src/FILE/ffunicode.o: ..\src\FILE\ffunicode.c ..\src\FILE\ff.h \
-  ..\src\FILE\ffconf.h
-..\src\FILE\ff.h:
-..\src\FILE\ffconf.h:
--- Debug/src/GUI/lvgl-9.2.1/src/stdlib/rtthread/lv_sprintf_rtthread.d
+++
@@ -1,8 +0,0 @@
-src/GUI/lvgl-9.2.1/src/stdlib/rtthread/lv_sprintf_rtthread.o: \
-  ..\src\GUI\lvgl-9.2.1\src\stdlib\rtthread\lv_sprintf_rtthread.c \
-  ..\src\GUI\lvgl-9.2.1\src\stdlib\rtthread\..\..\lv_conf_internal.h \
-  ..\src\GUI\lvgl-9.2.1\src\stdlib\rtthread\..\..\lv_conf_kconfig.h \
-  H:\MyDocument\project\ra8d1\newnewnew\src\GUI\lv_conf.h
-..\src\GUI\lvgl-9.2.1\src\stdlib\rtthread\..\..\lv_conf_internal.h:
-..\src\GUI\lvgl-9.2.1\src\stdlib\rtthread\..\..\lv_conf_kconfig.h:
-H:\MyDocument\project\ra8d1\newnewnew\src\GUI\lv_conf.h:
